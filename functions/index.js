d33881b57bb973b6aad1ec9977d9f20283c63552
David J Felix <felix.davidj@gmail.com>
2017-05-28 18:35:09 -0400

    Add pubsub function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,12 @@
 var functions = require('firebase-functions');
+var PubSub = require('@google-cloud/pubsub');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+exports.rawVideoNotifier = functions.storage.object().onChange(event => {
+  if (!event.name.startsWith('raw-videos/')) {
+    console.log('Not a video to be encoded');
+    return;
+  } else {
+    var pubSub = PubSub();
+    console.log(pubSub);
+  }
 });
