3664eddf61f5cddd6c345a1e5cdbccb602c846a9
David J Felix <felix.davidj@gmail.com>
2017-06-20 21:49:57 -0400

    Make the hacky video service work.
    - Really ought to move this into a bucket of its own
    - Research creating video bucket and setting cors for it properly

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,11 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const PubSub = require('@google-cloud/pubsub');
+const GCS = require('@google-cloud/storage');
+const express = require('express');
+
+// Initialize firebase admin using functions login
+admin.initializeApp(functions.config().firebase);
 
 exports.rawVideoNotifier = functions.storage.object().onChange(event => {
   console.log(event);
@@ -28,11 +33,33 @@ exports.videoNotifier = functions.storage.object().onChange(event => {
   }
 });
 
-exports.videos = functions.https.onRequest((req, resp) => {
-  console.log(req)
-  if (req.method !== 'GET') {
-    resp.status(405).send(); // TODO: error object?
-    return;
-  }
 
+// A passthrough service to get files from google storage and serve them with CORS
+const videos = express();
+videos.get('/:videoId/:fileName', (req, resp) => {
+  console.log(req)
+  resp.header('Access-Control-Allow-Origin', '*');
+  const gcs = GCS();
+  const bucket = gcs.bucket('iotv-1e541.appspot.com');
+  const fileName = `videos/${req.params.videoId}/${req.params.fileName}`
+  console.log(`Filename: ${fileName}`)
+  const file = bucket.file(fileName)
+  file.exists((err, exists) => {
+    if (err === null) {
+      if (exists) {
+        resp.status(200);
+        file.createReadStream().pipe(resp);
+      } else {
+        resp.status(404).send('Not Found');
+      }
+    } else {
+      console.log(err);
+      resp.status(500).send('Server Error')
+    }
+  });
 });
+
+// Default 404 handler to quickly end bad routes
+videos.use((req, resp) => resp.sendStatus(404));
+
+exports.videos = functions.https.onRequest(videos);
